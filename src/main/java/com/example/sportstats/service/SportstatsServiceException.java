ce20c608ba4115d25d65c98eca5581ce6cf102e3
1337swish <claes.and98@hotmail.com>
2020-11-28 17:12:27 +0100

    Added http status code field to SportstatsServiceException

--- src/main/java/com/example/sportstats/service/SportstatsServiceException.java
+++ src/main/java/com/example/sportstats/service/SportstatsServiceException.java
@@ -1,11 +1,15 @@
 package com.example.sportstats.service;
 
+import org.springframework.http.HttpStatus;
+
 /**
  * Exception class for sportstats services.
  *
  * @author Claes Andersson
  */
 public class SportstatsServiceException extends RuntimeException {
+
+    private HttpStatus httpStatusCode;
 
     public SportstatsServiceException(String message) {
 
@@ -20,5 +24,16 @@ public class SportstatsServiceException extends RuntimeException {
     public SportstatsServiceException(Throwable cause) {
 
         super(cause);
+    }
+
+    public SportstatsServiceException(String message, HttpStatus httpStatusCode) {
+
+        super(message);
+        this.httpStatusCode = httpStatusCode;
+    }
+
+    public HttpStatus getHttpStatusCode() {
+
+        return httpStatusCode;
     }
 }
